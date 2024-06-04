239bd5116409cce4f7443ca43c517fa2da8af5d3
KyllianCamp <campagnekyllian@gmail.com>
2024-06-04 18:38:54 +0200

    feat(javaFX)!: adding first scene

---
+++ src/main/java/Controller.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Controller {
+
+    @FXML
+    void onBtnClick(ActionEvent event) {
+        
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,24 @@
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
-
-public class Main {
+public class Main extends Application{
     public static void main(String[] args) {
-        
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
+        
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Untitled2.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
