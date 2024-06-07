05283e3726a986119b5a0a54ad8ef114c3fa8d09
Jeannedelcourtlonguepe <jeanne.delcourt-longuepe@ig2i.centralelille.fr>
2024-06-07 18:27:47 +0200

    Listes locations design

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/Biens.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("View/Locations.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
