65051b1be96807850fd160b7c7dd233cf4a5184a
KyllianCamp <campagnekyllian@gmail.com>
2024-06-09 23:17:05 +0200

    removing persistence.xml to not send everytime

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+
+import Location.Utilisateur;
+import io.github.cdimascio.dotenv.Dotenv;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -8,7 +11,13 @@ import javafx.scene.Scene;
 public class Main extends Application{
     public static void main(String[] args) {
         java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.OFF);
-        
+         // Chargez les variables d'environnement à partir du fichier .env
+        Dotenv dotenv = Dotenv.load();
+
+        Utilisateur utilisateur = new Utilisateur();
+        Utilisateur utilisateur2 = utilisateur.getUtilisateurById(1);
+
+
         launch(args);
     }
 
